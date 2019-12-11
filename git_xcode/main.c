070bf8b037ef8dd45f4e42d7a029193767fba3cb
손혁천 <interfacehand@gmail.com>
2019-12-11 14:04:35 +0900

    Initial Commit

---
+++ git_xcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  git_xcode
+//
+//  Created by 손혁천 on 2019/12/11.
+//  Copyright © 2019 손혁천. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
